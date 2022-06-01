e05e4d379676a517661a55eaf4d8d8998d83d06f
Syfe <22247810+ItsSyfe@users.noreply.github.com>
2022-06-01 16:06:39 +0100

    test

--- src/cmd/profile.js
+++ src/cmd/profile.js
@@ -30,42 +30,44 @@ module.exports = {
 	async execute(interaction) {
 		const username = interaction.options.getString('username');
 		await interaction.deferReply();
-		try {
-			const Account = await ApiHelper.getAccountFromUsername(username);
+		// try {
+		const Account = await ApiHelper.getAccountFromUsername(username);
 
-			const profileEmbed = await new MessageEmbed()
-				.setColor('#0099ff')
-				.setTitle(`${Account.name}${Account.plan == 'Plus' ? ' **+**' : ''}`)
-				.setURL(`https://dashboard.blooket.com/stats?name=${Account.name}`)
-				.setDescription(`__Details:__
+		const profileEmbed = await new MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle(`${Account.name}${Account.plan == 'Plus' ? ' **+**' : ''}`)
+			.setURL(`https://dashboard.blooket.com/stats?name=${Account.name}`)
+			.setDescription(`__Details:__
 				**Tokens:** ${abbreviateNumber(Account.tokens)}
 				**Total Tokens:** ${abbreviateNumber(Account.totalTokens)}
 				**Boxes Opened:** ${abbreviateNumber(Account.boxesOpened)} 
 				**Daily Token Available:** ${Account.tokensAvailable}
 				**Daily XP Available:** ${Account.xpAvailable}
 				**Time Before Reset:** <t:${Math.round(new Date().setHours(24, 0, 0, 0)) / 1000}:R>`)
-				.addFields(
-					{ name: 'Wins', value: `> ${Account.wins} (${Math.round(Account.wins / Account.gamesPlayed * 100)}% win rate)`, inline: true },
-					{ name: 'Showdown Wins', value: `> ${abbreviateNumber(Account.showdownWins)} wins`, inline: true },
-					{ name: 'Top 5s', value: `> ${Account.topFives}`, inline: true },
-					{ name: 'Players Defeated', value: `> ${abbreviateNumber(Account.playersDefeated)} players`, inline: true },
-					{ name: 'Correct Answers', value: `> ${abbreviateNumber(Account.correctAnswers)} answers`, inline: true },
-					{ name: 'Games Played', value: `> ${abbreviateNumber(Account.gamesPlayed)} games`, inline: true },
-					{ name: 'Total Points', value: `> ${abbreviateNumber(Account.classicPoints)} points`, inline: true },
-					{ name: 'Total Crypto', value: `> ${abbreviateNumber(Account.totalCrypto)} crypto`, inline: true },
-					{ name: 'Total Gold', value: `> ${abbreviateNumber(Account.totalGold)} gold`, inline: true },
-					{ name: 'Total Fish Weight', value: `> ${abbreviateNumber(Account.totalFishWeight)} fish weight`, inline: true },
-					{ name: 'Total Cash', value: `> ${abbreviateNumber(Account.totalCash)} cash`, inline: true },
-					{ name: 'Racing Progress', value: `> ${abbreviateNumber(Account.racingProgress)} progress`, inline: true },
-					{ name: 'Factory Upgrades', value: `> ${abbreviateNumber(Account.upgrades)} upgrades`, inline: true },
-				)
-				.setTimestamp(Date.parse(Account.dateCreated))
-				.setFooter(`Account ID: ${Account._id.toString()}`);
+			.addFields(
+				{ name: 'Wins', value: `> ${Account.wins} (${Math.round(Account.wins / Account.gamesPlayed * 100)}% win rate)`, inline: true },
+				{ name: 'Showdown Wins', value: `> ${abbreviateNumber(Account.showdownWins)} wins`, inline: true },
+				{ name: 'Top 5s', value: `> ${Account.topFives}`, inline: true },
+				{ name: 'Players Defeated', value: `> ${abbreviateNumber(Account.playersDefeated)} players`, inline: true },
+				{ name: 'Correct Answers', value: `> ${abbreviateNumber(Account.correctAnswers)} answers`, inline: true },
+				{ name: 'Games Played', value: `> ${abbreviateNumber(Account.gamesPlayed)} games`, inline: true },
+				{ name: 'Total Points', value: `> ${abbreviateNumber(Account.classicPoints)} points`, inline: true },
+				{ name: 'Total Crypto', value: `> ${abbreviateNumber(Account.totalCrypto)} crypto`, inline: true },
+				{ name: 'Total Gold', value: `> ${abbreviateNumber(Account.totalGold)} gold`, inline: true },
+				{ name: 'Total Fish Weight', value: `> ${abbreviateNumber(Account.totalFishWeight)} fish weight`, inline: true },
+				{ name: 'Total Cash', value: `> ${abbreviateNumber(Account.totalCash)} cash`, inline: true },
+				{ name: 'Racing Progress', value: `> ${abbreviateNumber(Account.racingProgress)} progress`, inline: true },
+				{ name: 'Factory Upgrades', value: `> ${abbreviateNumber(Account.upgrades)} upgrades`, inline: true },
+			)
+			.setTimestamp(Date.parse(Account.dateCreated))
+			.setFooter(`Account ID: ${Account._id.toString()}`);
 
-			await interaction.editReply({ content: null, embeds: [ profileEmbed ] });
+		await interaction.editReply({ content: null, embeds: [ profileEmbed ] });
+		/*
 		}
 		catch {
 			await interaction.editReply({ content: `Couldn't find a profile for user ${username}, are you sure that username is correct?`, ephemeral: true });
 		}
+		*/
 	},
 };
