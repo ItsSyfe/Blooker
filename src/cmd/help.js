94e4540a28f23026fe0433a0c78304a7efd0ed02
Syfe <22247810+ItsSyfe@users.noreply.github.com>
2022-07-23 13:53:15 +0100

    added help command

---
+++ src/cmd/help.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('📖 See all my commands!'),
+	async execute(interaction) {
+
+		const helpEmbed = await new MessageEmbed()
+			.setColor('#0cc3ce')
+			.setTitle('Blooker Help')
+			.setImage('https://i.imgur.com/FMy136Z.png')
+			.setDescription(`Blooker is a companion bot for [Blooket](https://blooket.com)! We provide useful utility commands that aid you in Blooket, please note that NONE of our commands break Blooket's ToS and we do not have ANY access to your account when using our bot.
+			
+			**▸ /help** Brings up this nifty help menu!
+
+			**▸ /profile** View your Blooket profile and stats! Simply use /profile or /profile <username> to view another user's profile!
+
+			**▸ /blooks** View all the Blooks you have unlocked! Use /blooks or /blooks <username> to view another user's Blooks!
+
+			**▸ /missingblooks** Ever wanted to see what Blooks you're missing? Use /missingblooks or /missingblooks <username> to view missing Blooks!
+
+			**▸ /link** Link your Blooket account to your Discord account! Use /link <username> to link your Discord account to your Blooket account, you can only do this in DMs with me!`);
+
+		await interaction.reply({ content: null, embeds: [ helpEmbed ], components: [ ] });
+	},
+};
